da7e9e4b36d1f287b34a06b28fbc6f086d516eb0
Nicole Szeto <nszzz@campus-103-146.ucdavis.edu>
2025-04-06 02:15:30 -0700

    Uploaded the frontend and backend files for moodco

---
+++ backend/src/index.ts
@@ -0,0 +1,38 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+
+const app = new Hono()
+app.use('*', cors())
+
+type Mood = {
+  emoji: string;
+  mood: string;
+}
+
+const moods: Mood[] = []; //Stores options that the user can click
+const moodHistory: Mood[] = []; //Stores the user's mood history
+
+app.get('/', (c) => {
+  return c.text('Hello! How are you feeling today?')
+})
+
+//Enters moods into the array, moodHistory
+app.post('/backend/index.ts', async (c) => {
+  const mood: Mood = await c.req.json()
+  moodHistory.push(mood)
+  return c.json({success: true})
+})
+
+//Returns the mood history to the userad
+app.get('/backend/index.ts', (c) => {
+  return c.json(moodHistory);
+})
+
+
+serve({
+  fetch: app.fetch,
+  port: 3000
+}, (info) => {
+  console.log(`Server is running on http://localhost:${info.port}`)
+})
